88edea67443d3c5e69e74738e28be3181b944c02
zkp <zkp@tju.edu.cn>
2023-02-04 20:25:39 +0800

    load full index

---
+++ ad-service/ad-search/src/main/java/github/zkpgeeker/ad/index/IndexFileLoader.java
@@ -0,0 +1,108 @@
+package github.zkpgeeker.ad.index;
+
+import com.alibaba.fastjson.JSON;
+import github.zkpgeeker.ad.dump.DConstant;
+import github.zkpgeeker.ad.dump.table.*;
+import github.zkpgeeker.ad.handler.AdLevelDataHandler;
+import github.zkpgeeker.ad.mysql.constant.OpType;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @description:
+ * @author: ZKP
+ * @time: 2023/2/4
+ */
+@Component
+@DependsOn("dataTable")
+public class IndexFileLoader {
+
+    /**
+     * 在启动时加载全量索引
+     */
+    @PostConstruct
+    public void init() {
+        //second-level
+        List<String> adPlanStrings = loadDumpData(
+                String.format("%s%s",
+                        DConstant.DATA_ROOT_DIR,
+                        DConstant.AD_PLAN)
+        );
+        adPlanStrings.forEach(p -> AdLevelDataHandler.handleLevel2(
+                JSON.parseObject(p, AdPlanTable.class),
+                OpType.ADD
+        ));
+        List<String> adCreativeStrings = loadDumpData(
+                String.format("%s%s",
+                        DConstant.DATA_ROOT_DIR,
+                        DConstant.AD_CREATIVE)
+        );
+        adCreativeStrings.forEach(c -> AdLevelDataHandler.handleLevel2(
+                JSON.parseObject(c, AdCreativeTable.class),
+                OpType.ADD
+        ));
+       // third-level
+        List<String> adUnitStrings = loadDumpData(
+                String.format("%s%s",
+                        DConstant.DATA_ROOT_DIR,
+                        DConstant.AD_UNIT)
+        );
+        adUnitStrings.forEach(u -> AdLevelDataHandler.handleLevel3(
+                JSON.parseObject(u, AdUnitTable.class),
+                OpType.ADD
+        ));
+        List<String> adCreativeUnitStrings = loadDumpData(
+                String.format("%s%s",
+                        DConstant.DATA_ROOT_DIR,
+                        DConstant.AD_CREATIVE_UNIT)
+        );
+        adCreativeUnitStrings.forEach(cu -> AdLevelDataHandler.handleLevel3(
+                JSON.parseObject(cu, AdCreativeUnitTable.class),
+                OpType.ADD
+        ));
+        // fourth-level
+        List<String> adUnitDistrictStrings = loadDumpData(
+                String.format("%s%s",
+                        DConstant.DATA_ROOT_DIR,
+                        DConstant.AD_UNIT_DISTRICT)
+        );
+        adUnitDistrictStrings.forEach(ud -> AdLevelDataHandler.handleLevel4(
+                JSON.parseObject(ud, AdUnitDistrictTable.class),
+                OpType.ADD
+        ));
+        List<String> adUnitItStrings = loadDumpData(
+                String.format("%s%s",
+                        DConstant.DATA_ROOT_DIR,
+                        DConstant.AD_UNIT_IT)
+        );
+        adUnitItStrings.forEach(ui -> AdLevelDataHandler.handleLevel4(
+                JSON.parseObject(ui, AdUnitItTable.class),
+                OpType.ADD
+        ));
+        List<String> adUnitKeywordStrings = loadDumpData(
+                String.format("%s%s",
+                        DConstant.DATA_ROOT_DIR,
+                        DConstant.AD_UNIT_KEYWORD)
+        );
+        adUnitKeywordStrings.forEach(uk -> AdLevelDataHandler.handleLevel4(
+                JSON.parseObject(uk, AdUnitKeywordTable.class),
+                OpType.ADD
+        ));
+    }
+
+    private List<String> loadDumpData(String fileName) {
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(fileName))) {
+            return br.lines().collect(Collectors.toList());
+        } catch (IOException ex) {
+            throw new RuntimeException(ex.getMessage());
+        }
+    }
+}
