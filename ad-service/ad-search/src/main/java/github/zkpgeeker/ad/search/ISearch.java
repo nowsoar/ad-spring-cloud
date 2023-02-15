c8d8e700afb85e457688dff44333102610c1e434
zkp <zkp@tju.edu.cn>
2023-02-15 22:47:03 +0800

    search response dto

--- ad-service/ad-search/src/main/java/github/zkpgeeker/ad/search/ISearch.java
+++ ad-service/ad-search/src/main/java/github/zkpgeeker/ad/search/ISearch.java
@@ -1,9 +1,14 @@
 package github.zkpgeeker.ad.search;
 
+import github.zkpgeeker.ad.search.vo.SearchRequest;
+import github.zkpgeeker.ad.search.vo.SearchResponse;
+
 /**
  * @description:
  * @author: ZKP
  * @time: 2023/2/13
  */
 public interface ISearch {
+
+    SearchResponse fetchAds(SearchRequest request);
 }
